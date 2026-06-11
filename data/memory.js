e56229df534ea1c713fbf6ffaee61cdbfa3b6a84
Herik Thalisson <thalisson.herik@outlook.com>
2026-06-11 16:23:05 -0300

    Update memory entries with new titles and dates

--- data/memory.js
+++ data/memory.js
@@ -2,9 +2,18 @@ export const titulo = "Algumas Memórias 💭"
 
 export const memorias = [
   {
-    titulo: 'Nosso primeiro encontro',
-    data: 'Meados de Outubro de 2023',
-    descricao: 'Nesse fatídico dia, fomos comer no OutBack ali estavamos dando definitivamente o pontepé inicial.',
+    titulo: 'Quando começamos a nos falar',
+    data: 'Meados de 2015',
+    descricao: 'Começamos a conversar e eu não imaginava que você seria uma pessoa tão importante.',
+  },
+  {
+    titulo: 'Nosso primeiro Clash',
+    data: 'Meados de 2024',
+    descricao: 'Nesse dia, fomos campeões do clash jogando duo no bot.',
+  },{
+    titulo: 'Nosso primeiro dia dos namorados',
+    data: '12 de outubro de 2015',
+    descricao: 'Nosso primeiro dia dos namorados juntos, e espero que venham bem mais do que só esses, e espero que os proximos passemos realmente juntos',
   },
   // continue com os momentos...
 ]
